1653f235ffc685e36ba85eaa9a68ce66e6e16e31
Foxylab <rushan.shakirzianov@gmail.com>
2022-07-05 19:26:58 +0300

    feat: start working with api

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,12 +1,19 @@
 import Header from "./Header";
 import Sign from "./Sign";
 
-const Login = () => {
-  
+const Login = (props) => {
   return (
     <>
       <Header link={"/sign-up"} linkText={"Регистрация"} />
-      <Sign header="Вход" buttonText="Войти" hidden={true} />
+      <Sign
+        header="Вход"
+        buttonText="Войти"
+        hidden={true}
+        handleSubmit={props.onLogin}
+        passwordInput={props.passwordInput}
+        emailInput={props.emailInput}
+        handleChangeInput={props.handleChangeInput}
+      />
     </>
   );
 };
--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -1,19 +1,7 @@
 import React from "react";
 import { Outlet, Navigate } from "react-router-dom";
 
-/*export default function ProtectedRoute({ component: Component, ...props }) {
-    return (
-        <Route>
-          {() =>
-            props.loggedIn ? <Component {...props} /> : <Navigate to="/sign-in" />
-          }
-        </Route>
-      );
-}*/
-
 const ProtectedRoute = (props) => {
-  // If authorized, return an outlet that will render child elements
-  // If not, return element that will navigate to login page
   return props.loggedIn ? <Outlet /> : <Navigate to="/sign-in" />;
 };
 
